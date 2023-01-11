6967f75b22aa51ddcb80bff534963b2e932abffc
GiovanniMarcinari <gmarcinari@gmail.com>
2023-01-11 14:56:48 -0300

    Desenvolvimento inicial da classe cálculos.

---
+++ Calculos.java
@@ -0,0 +1,19 @@
+public class Calculos {
+    public double adicao(double valor1, double valor2) {
+        return valor1 + valor2;
+    }
+
+    public double subtracao(double valor1, double valor2) {
+        return valor1 - valor2;
+
+    }
+    public double multiplicacao (double valor1, double valor2) {
+        return valor1 * valor2;
+
+    }
+    public double divisao (double valor1, double valor2) {
+        return valor1 / valor2;
+
+    }
+
+}
